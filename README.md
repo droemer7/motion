e5168530cc17b14c7161580218e4d8c33de96a15
Dane Roemer <droemer7@gmail.com>
2023-02-19 13:34:16 -0800

    Add ROS package and update README

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# motion
+
+
+# Motion
+
 A collection of mobile robot trajectory generation algorithms for path and velocity profiles, integrated with ROS.
+
+**Author:** Dane Roemer
+
+</br>
+
+There is nothing here right now, other than scratch test code for testing the optimization library [CppNumericalSolvers (fork as ROS package)](https://github.com/droemer7/CppNumericalSolvers) (which this repo will utilize).
+
+</br>
+
+## References
+
+1. [MuSHR (site)](https://mushr.io) | [MuSHR (github)](https://github.com/prl-mushr) for building the car and getting an introduction to ROS.
+2. [Coursera - Self Driving Cars](https://www.coursera.org/specializations/self-driving-cars) for cubic spiral optimization formulation.
+3. [[CppNumericalSolvers](https://github.com/PatWie/CppNumericalSolvers) for the numerical solver.
---
+++ trajectory_generator/src/main.cpp
@@ -0,0 +1,84 @@
+// Copyright 2020, https://github.com/PatWie/CppNumericalSolvers
+
+#include <iostream>
+#include <limits>
+
+#include <ros/ros.h>
+
+#include "cppoptlib/function.h"
+#include "cppoptlib/solver/bfgs.h"
+#include "cppoptlib/solver/conjugated_gradient_descent.h"
+#include "cppoptlib/solver/gradient_descent.h"
+#include "cppoptlib/solver/lbfgs.h"
+#include "cppoptlib/solver/lbfgsb.h"
+#include "cppoptlib/solver/newton_descent.h"
+
+using FunctionXd = cppoptlib::function::Function<double>;
+
+class Function : public FunctionXd {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+  using FunctionXd::hessian_t;
+  using FunctionXd::vector_t;
+
+  scalar_t operator()(const vector_t &x) const override {
+    return 5 * x[0] * x[0] + 100 * x[1] * x[1] + 5;
+  }
+
+  void Gradient(const vector_t &x, vector_t *grad) const override {
+    (*grad)[0] = 2 * 5 * x[0];
+    (*grad)[1] = 2 * 100 * x[1];
+  }
+
+  void Hessian(const vector_t &x, hessian_t *hessian) const override {
+    (*hessian)(0, 0) = 10;
+    (*hessian)(0, 1) = 0;
+    (*hessian)(1, 0) = 0;
+    (*hessian)(1, 1) = 200;
+  }
+};
+
+int main(int argc, char** argv) {
+  // Initialize ROS
+  ros::init(argc, argv, "profile");
+  ros::NodeHandle nh;
+
+  // using Solver = cppoptlib::solver::NewtonDescent<Function>;
+  // using Solver = cppoptlib::solver::GradientDescent<Function>;
+  // using Solver = cppoptlib::solver::ConjugatedGradientDescent<Function>;
+  // using Solver = cppoptlib::solver::Bfgs<Function>;
+  // using Solver = cppoptlib::solver::Lbfgs<Function>;
+  using Solver = cppoptlib::solver::Lbfgsb<Function>;
+
+  constexpr auto dim = 2;
+  Function f;
+  Function::vector_t x(dim);
+  x << -1, 2;
+
+  auto state = f.Eval(x);
+  std::cout << "this" << std::endl;
+
+  std::cout << f(x) << std::endl;
+  std::cout << state.gradient << std::endl;
+  if (state.hessian) {
+    std::cout << *(state.hessian) << std::endl;
+  }
+
+  std::cout << cppoptlib::utils::IsGradientCorrect(f, x) << std::endl;
+  std::cout << cppoptlib::utils::IsHessianCorrect(f, x) << std::endl;
+
+  Solver solver{
+      Function::vector_t::Constant(
+          dim, std::numeric_limits<typename Function::scalar_t>::lowest()),
+      Function::vector_t::Constant(
+          dim, std::numeric_limits<typename Function::scalar_t>::max())};
+
+  auto [solution, solver_state] = solver.Minimize(f, x);
+  std::cout << "argmin " << solution.x.transpose() << std::endl;
+  std::cout << "f in argmin " << solution.value << std::endl;
+  std::cout << "iterations " << solver_state.num_iterations << std::endl;
+  std::cout << "status " << solver_state.status << std::endl;
+
+  return 0;
+}
